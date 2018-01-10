ee7f849914388fbca3d2832537d5700eb1fe6a05
amrrs <amrrs@outlook.com>
2018-01-10 14:36:15 +0530

    adding more test case to see code cov improvmenet

--- tests/testthat/test-fun.R
+++ tests/testthat/test-fun.R
@@ -7,3 +7,14 @@ test_that("The output dataframe Type is ",{
         expect_equal(class(get_global_marketcap('EUR')),"data.frame")
 
 })
+
+context("get_marketcap_ticker_all Output DF Check")
+
+test_that("The output dataframe Type is ",{
+
+        expect_equal(class(get_marketcap_ticker_all('USD')),"data.frame")
+        expect_equal(class(get_marketcap_ticker_all('EUR')),"data.frame")
+
+})
+
+

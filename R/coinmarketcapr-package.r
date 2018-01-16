cf4ec9810bd970c8d3a36be700d01335cfb6ad28
amrrs <amrrs.data@gmail.com>
2018-01-17 01:11:06 +0530

    package document

---
+++ R/coinmarketcapr-package.r
@@ -0,0 +1,16 @@
+#' coinmarketcapr: Get Cryptocurrencies Market Cap Prices from CoinMarketCap
+#'
+#' @description
+#' To extract and monitor price and market cap of 'Cryptocurrencies'
+#' from 'CoinMarketCap' <https://coinmarketcap.com/api/>.
+#'
+#' @section See Also:
+#' Useful links:
+#' * [https://coinmarketcap.com/api/](https://coinmarketcap.com/api/)
+#' * [https://github.com/amrrs/coinmarketcapr](https://github.com/amrrs/coinmarketcapr)
+#' * Report bugs at [https://github.com/amrrs/coinmarketcapr/issues](https://github.com/amrrs/coinmarketcapr/issues)
+#'
+#' @docType package
+#' @name coinmarketcapr
+#' @md
+NULL

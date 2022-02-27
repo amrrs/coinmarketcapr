a62665844d74d2acf90c55b47bdf2d55ccafd651
amrrs <amrrs@outlook.com>
2022-02-27 11:03:01 +0000

    fixed plot to use the crypto-listing function

--- R/plot.R
+++ R/plot.R
@@ -20,26 +20,27 @@ plot_top_currencies <- function(currency = "USD", k = 5, bar_color = "grey") {
         stop(cat(crayon::red(cli::symbol$cross,
                              "Parameter k must be a integer value greater than zero.\n")))
 
-    temp <- curl_fetch_memory(
-        paste0("https://api.coinmarketcap.com/v1/ticker/?convert=",
-               currency))
-    check_response(temp)
-    temp <- data.frame(fromJSON(rawToChar(temp$content)))
+    # temp <- curl_fetch_memory(
+    #     paste0("https://api.coinmarketcap.com/v1/ticker/?convert=",
+    #            currency))
+    # check_response(temp)
+    # temp <- data.frame(fromJSON(rawToChar(temp$content)))
+
+    temp <- coinmarketcapr::get_crypto_listings(currency = currency, limit = k)
 
     if (k > nrow(temp))
         warning(cat(crayon::yellow(cli::symbol$warning,
                                    "The argument provided to k is greater than the",
                                    "number of cryptocurrencies. Only", 4, "will be plotted.\n")))
 
-    temp <- temp[seq_len(min(k, nrow(temp))), ]
+    # temp <- temp[seq_len(min(k, nrow(temp))), ]
+    #
+    # temp[, tolower(paste0("price_", currency))] <- round(
+    #     as.numeric(temp[, tolower(paste0("price_", currency))]), 2)
 
-    temp[, tolower(paste0("price_", currency))] <- round(
-        as.numeric(temp[, tolower(paste0("price_", currency))]), 2)
-
-    ggplot(temp, aes_string("name", tolower(paste0("price_", currency)))) +
-        geom_bar(stat = "identity", fill = bar_color) +
+    ggplot(temp, aes_string("name", paste0(toupper(currency),"_price"))) +
+        geom_col(fill = bar_color) +
         ylab(paste0("Price in ", currency)) +
         xlab("Cryptocurrencies") +
-        ggtitle(paste0("Top ", k, " Cryptocurrencies with Largest Marketcaps")) +
-        coord_flip()
+        ggtitle(paste0("Top ", k, " Cryptocurrencies with Largest Marketcaps"))
 }
